8019e0018eb1f92522cd270379a3dbfcc922c0f4
Sebastian Soto <s.m.sebastian.n@gmail.com>
2020-09-11 12:48:49 -0600

    Fix: font

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"FiraCode Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
