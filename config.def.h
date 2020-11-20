25d276d3110d2856be707977cfe031630da1f163
Sebastian Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-11-20 17:58:24 -0600

    fix: formatted code

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"FiraCode Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -30,4 +30,4 @@ static const char worddelimiters[] = " ";
 static unsigned int preselected = 0;
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
